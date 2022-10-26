4b12dc65c6bc1692f3856d583d70c45e2f210c36
T1PHA <tiphaelflak@gmail.com>
2022-10-26 22:05:36 +0200

    ajout fonctions lire ecrire

--- openfile.py
+++ openfile.py
@@ -1,36 +1,47 @@
-#Lecture du fichier
+
 import csv
-with open('conso-annuelles_v1.csv',newline='') as f:
-    tableau=[]
-    lire=csv.reader(f)
-    print('',end='\n')
-    print('Affichage des lignes du tableau',end='\n')
-    # -> test tri //  print("Sorting based on the type:\n", sorted(tableau, key=lambda i: i['Type']))
-    
-    for ligne in lire:
-        print(ligne,end='\n')
-        tableau.append(ligne)
 
-#Création d'un fichier à partir de la liste tableau
-with open('conso-clean.csv','w',newline='') as f:  #Ouverture du fichier CSV en écriture
-    ecrire=csv.writer(f)                        # préparation à l'écriture
-    for i in tableau:                           # Pour chaque ligne du tableau...
-        ecrire.writerow(i)                # Mettre dans la variable ecrire cette nouvelle ligne
-print('',end='\n')
-print('longueur du tableau : ',len(tableau))
 
-#Tri fichier
-    # sorted_tab = sorted(tableau, key=lambda x: x[4])
-    #     print(sorted_tab)
-        
-#         tableau = list(lire)
+tableau_init_csv = "conso-annuelles_v1.csv"
 
-# for conso in tableau:
-#     conso['Type'] = int(conso['Type'])
+#tableau_init_csv.to_csv(f'conso-clean.csv',index =False, sep= ';', header = True, na_rep="")
+tableau_clean_csv = "conso-clean.csv"
 
-# tableau.sort(key= lambda x: x['Type'])
 
-# # affichage:
-# for conso in conso:
-#     print(conso['Appareil suivi'], conso['ID logement'], conso['Consommation annuelle AN1'], conso['Consommation annuelle AN2'], conso['Type'])
 
+#lire et écrire sur notre variable tableau
+def lire_ecrire_csv():
+    #tableau csv qui prends les données finales
+    tableau_clean = []
+    #Ouverture tableau init afin de lire ses données
+    with open(tableau_init_csv, 'r') as csv_file:
+        lire = csv.reader(csv_file, delimiter = ';')
+        #Mettre les données du tableau init dans notre variable tableau
+        for ligne in lire:
+            print(ligne,end='\n')
+            tableau_clean.append(ligne)
+            
+    #        
+    #Fonctions parsing print a mettre
+    #
+    #Ouverture et création tableau clean en mettant à jour notre variable tableau
+    #en mettant la variable tableau clean csv, il ne connait pas le fichier qui lui ait associé (normal puisque il n'existe pas), il le créer
+    with open(tableau_clean_csv, 'w', newline='') as csv_file:
+    #Ecriture dans nouveau tableau
+        ecrire = csv.writer(csv_file)
+    #Mise à jour de notre variable tableau
+        ecrire.writerows(tableau_clean)
+lire_ecrire_csv()
+
+
+
+#def tri
+
+
+
+
+#def supprimer lignes vides
+
+
+
+# def combiner colonnes
