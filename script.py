8238aeb9e1fcd80e47df38460de1af5fa55a7214
T1PHA <tiphaelflak@gmail.com>
2022-12-04 21:06:44 +0100

    ajout tri & push final

--- script.py
+++ script.py
@@ -16,12 +16,12 @@ count = 0
         
 
     
-# methodes qu'on va apeller a la fin pour pouvoir ecrire dans le fichier final
+# fonction qu'on va apeller a la fin pour pouvoir ecrire dans le fichier final
 def ecrire_csv(tableau_clean):
 
     with open(tableau_clean_csv, 'w', newline='', encoding="utf-8") as csv_file:
         #On déclare l'en tête qu'on écrira a la fin dans le fichier
-        fieldnames=['Appareil suivi','Consommation annuelle AN1+AN2','Type']
+        fieldnames=['Appareil suivi | ' 'Consommation annuelle AN1+AN2 | ' 'Type']
         
         ecrire= csv.DictWriter(csv_file, fieldnames=fieldnames)
         ecrire.writeheader()
@@ -49,12 +49,12 @@ def colonnes(tableau_clean):
     #len_tab = len(tableau_clean)
     IndexLigne=1
     for ligne in tableau_clean:
-        # si une case est vide, on ne lui exécute pas la suite du code (elle est donc mise de coté)
+        # si une case est vide, on ne lui exécute pas la suite du code (elle est donc mise de coté) carr il prend seulement les IndexLigne = 0
         if len(ligne) == 0:
             IndexLigne = 0
 
     
-                 
+    # count = 0 pour exécuter le code qu'une fois      
     if IndexLigne==1 and count == 0:
         #On déclare les variables qui vont remplacer les colonnes
         l1 = tableau_clean[1].replace(',','.').replace('-','0')
@@ -64,7 +64,7 @@ def colonnes(tableau_clean):
         consoN12 = float(l1) + float(l2)
         
         # On supprime les 2 colonnes an1 et an2 pour les remplacer par une seule qui contient la fusion des deux
-        # 2 fois 1 car la colonne se décale et devient 1
+        # 2 fois pop(1) car la colonne se décale et devient position 1 dans le tableau
         tableau_clean.pop(1) and tableau_clean.pop(1) and tableau_clean.insert(1, consoN12)
 
         return tableau_clean
@@ -79,20 +79,24 @@ def lire_csv():
     
     with open(tableau_init_csv, 'r', encoding="utf-8") as csv_file:
         
-        lire = csv.reader(csv_file, delimiter = ';')
-        
-        # pour lire toutes les lignes   
+        lire = csv.reader(csv_file, delimiter = ';')  
         next(lire, 0)
         
-        #Pour chaques lignes du fichier de base : 
+        #Va exécuter mes fonctions pour chaque ligne de mon tab leau 
         for i in lire:
-            #On met la fonction principale dans une variable pour pouvoir l'exécuter
+            #On met la fonction principale dans une variable pour pouvoir l'exécuter sur toutes les lignes
             test = colonnes(i)
-            #On spécifie que le type soit bien une liste pour pouvoir opérer sur la variable
-            if type(test) is list:
-                #On ajoute au tableau notre variable qui va exécuter notre fonction
-                tableau_clean.append(test)
-                ###
+            
+                
+            if count == 0:
+                #On spécifie que le type soit bien une liste pour pouvoir opérer sur la variable
+                if type(test) is list:
+                    #On ajoute au tableau notre variable qui va exécuter notre fonction
+                    tableau_clean.append(test)
+                    #Tri d'abord par Type en  ordre décroissant puis par conso par ordre décroissant
+                    tableau_clean.sort(key=lambda x: (x[2], x[1]), reverse=True)
+            else: 
+                print("erreur")
             
     #Ecrire dans le nouveau csv avec les donnees de tableau_clean selon les opérations précédentes       
     ecrire_csv(tableau_clean)
